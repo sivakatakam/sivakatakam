959f7ecba7b9b5da992ad6ae1a3a2fe33deea173
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-03-11 21:03:42 +0530

    Add files via upload

---
+++ mydetails.java
@@ -0,0 +1,12 @@
+class details
+{
+	public static void main(String args[])
+	{
+		System.out.println("Name : K.Siva");
+		System.out.println("Roll No. : 22MH1A04H2");
+		System.out.println("College : ACOE");
+		System.out.println("Branch : ECE");
+		System.out.println("Batch : 2022 - 2026");
+		System.out.println("Thub : yes");
+	}
+}
